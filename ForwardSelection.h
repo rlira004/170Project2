8c2ac343ea6c6a1c91d284561ec13493f44786d7
Ricardo <63069526+rlira004@users.noreply.github.com>
2021-06-13 15:27:20 -0700

    Update ForwardSelection.h

--- ForwardSelection.h
+++ ForwardSelection.h
@@ -49,84 +49,11 @@ public:
 		}
 		else{
 			temp = 0;
-			while(randomNums.size() > 4){
-				for(int j = 0; j < randomNums.size(); j++){
+			for(int j = 0; j < randomNums.size(); j++){
 					if(randomNums[j]>temp){
 						temp = randomNums[j];
-						pos = j;
 					}
-				cout << j << endl;
-				}
-					//accuracy = temp;
-					//output.push_back(pos+1);
-					randomNums.erase(randomNums.begin()+pos);
-					//for(int k = 0; k <= randomNums.size(); k++){
-						//cout << position[k] << ") " << randomNums[k] << endl;
-					//}
 			}
-			
-			//cout << accuracy << endl;
-			//for(int x = 0; x < output.size(); x++){
-				//cout << output[x] << endl;
-			//}
 		}
 	}
 };
-
-
-
-/*for (int j = 0; j < num_of_features; j++) {
-	
-			cout << "Using features at " << "{" << j+1 << "}" << " accuracy is " << randomNums[j] << "%" << endl;
-		}
-		double temp = randomNums[0];
-		int pos;
-		for (int i = 0; i < num_of_features; i++) {
-			if (temp < randomNums[i]) {
-				temp = randomNums[i];
-				pos = i;
-			}void Gen_Ran() {
-		/* initialize random seed: */
-		/*
-		srand(time(NULL));
-		double ranNum;
-		for (int i = 0; i < num_of_features; i++) {
-			/* generate secret number between 1 and 10: *//*
-			ranNum = rand() % 1000 + 1;
-			randomNums.push_back(ranNum/10);
-		}
-		for (int j = 0; j < num_of_features; j++) {
-	
-			cout << "Using features at " << "{" << j+1 << "}" << " accuracy is " << randomNums[j] << "%" << endl;
-		}
-		double temp = randomNums[0];
-		int pos;
-		for (int i = 0; i < num_of_features; i++) {
-			if (temp < randomNums[i]) {
-				temp = randomNums[i];
-				pos = i;
-			}
-			if (
-			if (i == (num_of_features - 1) && temp == randomNums[0]) {
-				position.push_back(1);
-			}
-			else if (i == (num_of_featurehttps://github.com/rlira004/170Project2.gits - 1)) {
-				position.push_back(pos+1);
-			}
-		}
-
-		cout << endl << "Feature Set " << "{";
-			
-			for (int j = 0; j < position.size(); j++) {
-				cout << position[j];
-			}
-			cout << "}" << " was best, accuracy is " << temp << "%" << endl;
-	}
-
-
-
-
-
-};
-
-*/
