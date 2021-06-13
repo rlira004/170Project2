f23c6b38ff3ad61a6a3d264e65c4c1785b17785d
Ricardo <63069526+rlira004@users.noreply.github.com>
2021-06-13 14:24:43 -0700

    Update ForwardSelection.h

--- ForwardSelection.h
+++ ForwardSelection.h
@@ -60,7 +60,7 @@ public:
 					randomNums.erase(randomNums.begin()+pos);
 					position.erase(position.begin()+pos);
 					for(int k = 0; k < randomNums.size(); k++){
-						cout << position[k] << randomNums[k] << endl;
+						cout << position[k] << ") " << randomNums[k] << endl;
 					}
 			}
 		}
