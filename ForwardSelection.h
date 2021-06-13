c4f06fe59488b1534f096af22b45423a6150ad97
Ricardo <63069526+rlira004@users.noreply.github.com>
2021-06-13 10:47:59 -0700

    Update ForwardSelection.h

--- ForwardSelection.h
+++ ForwardSelection.h
@@ -30,7 +30,7 @@ public:
 			/* generate secret number between 1 and 10: */
 			ranNum = rand() % 1000 + 1;
 			randomNums.push_back(ranNum/10);
-			if(randomNums(i) > noFeatures)
+			if(randomNums[i] > noFeatures)
 				proceedSearching = true;
 		}
 		while(!randomNums.empty()){
